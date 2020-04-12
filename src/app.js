a797ef00690b0cf05858706854c064ef135fb3f7
DineshMittal1618 <dineshmittal1618@gmail.com>
2020-04-12 23:37:50 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs=require('hbs');
 const geoCode=require('./utils/geocode');
 const forecast=require('./utils/forecast')
 const app=express();
-
+const port=process.env.PORT || 3000;
 const pathName=path.join(__dirname,'../public');
 const viewPath=path.join(__dirname,'../templates/views');
 const partialPath=path.join(__dirname,'../templates/partials')
@@ -98,6 +98,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server started on port 3000');
+app.listen(port,()=>{
+    console.log('Server started on port '+port);
 })
