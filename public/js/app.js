baf41125b30963c5d8836d49d08bb054161b793c
DineshMittal1618 <dineshmittal1618@gmail.com>
2020-04-12 21:55:36 +0530

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('Client side javscript File is Loaded');
-
-
-
 const weatherForm=document.querySelector('form');
 const search=document.querySelector('input');
 const messageOne=document.querySelector('#message-1');
